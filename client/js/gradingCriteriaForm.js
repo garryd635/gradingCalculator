78ab65f6eb60340575307c2ddb21733c31a4d969
garryd635 <garryd635@gmail.com>
2015-11-23 20:54:07 -0500

    Percentage Validation on grading criteria form working

--- client/js/gradingCriteriaForm.js
+++ client/js/gradingCriteriaForm.js
@@ -15,22 +15,25 @@
 
 
 if (Meteor.isClient){
-
 	Meteor.startup(function() {
-	$('#percentage-error').hide()
 	console.log("Start up")
 });
 	
 	Template.gradingCriteriaForm.events({		
 		 'click #submit-button': function(){
-		 	var homeworkPercent = $('#homework-input').val()
-		 	var quizPercent = $('#quiz-input').val()
-			var examPercent = $('#exam-input').val()
-			var total = $(homeworkPercent+quizPercent+examPercent)
-			console.log(total)
+		 	var homeworkPercent = parseInt($('#homework-input').val())
+		 	var quizPercent = parseInt($('#quiz-input').val())
+			var examPercent = parseInt($('#exam-input').val())
+			var total = (homeworkPercent+quizPercent+examPercent)
+			console.log(total);
 			if(total != 100){
-				console.log("Not 100%");
+				console.log("Not 100%")
+				console.log(total)
+				$('#percentage-error').fadeIn("slow")
 			}	
+			else{
+				$('#percentage-error').hide()
+			}
         console.log(homeworkPercent);
 
     }
