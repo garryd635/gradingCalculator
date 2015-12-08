6216939c922c9f9af3479d46f99504b83b699202
garryd635 <garryd635@gmail.com>
2015-12-08 01:03:44 -0500

    Added forms, added criteria into collections when criteria is inputed

--- client/js/gradingCriteriaForm.js
+++ client/js/gradingCriteriaForm.js
@@ -17,13 +17,42 @@ if (Meteor.isClient){
 	console.log("Start up")
 });
 	
-	Template.gradingCriteriaForm.events({		
+Template.gradingCriteriaForm.onRendered( function(){
+	// $('#homework-input').hide()
+	// $('#quiz-input').hide()
+	// $('#exam-input').hide()
+	// $('#midterm-input').hide()
+	// $('#final-input').hide()
+	// $('#other1-input').hide()
+	// $('#other2-input').hide()
+
+	// $('#homework-label').hide()
+	// $('#quiz-label').hide()
+	// $('#exam-label').hide()
+	// $('#midterm-label').hide()
+	// $('#final-label').hide()
+	// $('#other1-label').hide()
+	// $('#other2-label').hide()
+})
+
+Template.gradingCriteriaForm.events({	
+			'click #update-button' : function(){
+
+			},
+
 		 'click #submit-button': function(){
 		 	var homeworkPercent = parseInt($('#homework-input').val())
 		 	var quizPercent = parseInt($('#quiz-input').val())
 			var examPercent = parseInt($('#exam-input').val())
-			var total = (homeworkPercent+quizPercent+examPercent)
+			var midtermPercent = parseInt($('#midterm-input').val())
+			var finalPercent = parseInt($('#final-input').val())
+			var other1Percent = parseInt($('#other1-input').val())
+			var other2Percent = parseInt($('#other2-input').val())
+
+			var total = (homeworkPercent+quizPercent+examPercent+midtermPercent+
+				finalPercent+other1Percent+other2Percent)
 			console.log(total);
+
 			if(total != 100){
 				console.log("Not 100%")
 				console.log(total)
@@ -32,9 +61,50 @@ if (Meteor.isClient){
 			}	
 			else{
 				$('#percentage-error').hide()
+
+				var criteria = [
+				{
+					name: "hwPercent",
+					percentage: homeworkPercent
+
+				},
+					
+						{
+							name: "quizPercent",
+						percentage: quizPercent
+					},
+
+					 {
+					 	name: "examPercent",
+						percentage: examPercent
+					 },
+						
+					 {
+					 	name: "midtermPercent",
+						percentage: midtermPercent
+					 },
+					  {
+					  name: "finalPercent",
+						percentage: finalPercent	
+					  },
+						
+					  {
+					  	name: "other1Percent",
+						percentage: other1Percent
+					  },
+
+					  {
+					  	name: "other2Percent",
+						percentage: other2Percent
+					  }			
+				];
+				
+				_.each(criteria, function(grades){
+					gradeCollection.insert(grades)
+				});
+
 				window.location.href = "/assignmentForm";
 			}
-        console.log(homeworkPercent);
 
     }
 	})	
